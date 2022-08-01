b115db9584bddc2d35db01185a5e39a72abe2341
mirzinho <mirzinho@gmail.com>
2022-08-01 11:55:25 +0200

    Event handler example

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'shell';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    window.addEventListener('shellEvent', this.eventHandler)
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('shellEvent', this.eventHandler)
+  }
+
+  eventHandler = (event: Event): void => {
+    console.log('eventHandler', event)
+  }
 }
